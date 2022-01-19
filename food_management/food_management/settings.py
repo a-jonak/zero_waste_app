5404ca51738785d7e81a06d43f7df3c23756bbd1
Agnieszka Jonak <agnieszka.jonak@gmail.com>
2022-01-19 16:09:32 +0100

    Remove a hardcoded Django secret key

--- food_management/food_management/settings.py
+++ food_management/food_management/settings.py
@@ -22,7 +22,6 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-2(j3!!hq$+d+gnwa1@5*troi9l7738f+4p&j^=cop)sbwyt3l+'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
