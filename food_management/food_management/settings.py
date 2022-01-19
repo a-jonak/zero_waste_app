0f661ef62dcb86b19127a5b53c0bf9362514a8e9
Agnieszka Jonak <agnieszka.jonak@gmail.com>
2022-01-19 15:34:13 +0100

    Changes in settings and Procfile'

--- food_management/food_management/settings.py
+++ food_management/food_management/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/3.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
-import dj_database_url
+# import dj_database_url
 import django_heroku
 import os
 from pathlib import Path
@@ -85,8 +85,8 @@ DATABASES = {
         'PORT': '5432',
     }
 }
-db_from_env = dj_database_url.config(conn_max_age=600, ssl_require=True)
-DATABASES['default'].update(db_from_env)
+# db_from_env = dj_database_url.config(conn_max_age=600, ssl_require=True)
+# DATABASES['default'].update(db_from_env)
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
