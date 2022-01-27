ed815794cfb4cce0341161fdae8d0bccf88cdd4e
Agnieszka Jonak <agnieszka.jonak@gmail.com>
2022-01-27 16:59:38 +0100

    Small corrections

--- food_management/settings.py
+++ food_management/settings.py
@@ -20,6 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
+SECRET_KEY = ')j&-()z(k)_g9295u!q4goztz0af%%o0p9isr3v5s#1=fhys%3'
 
 # SECURITY WARNING: keep the secret key used in production secret!
 
